31e1b38a700e25eeddcca8fa2e45c7d9e58a7fb9
Markku Åkerfelt <maraker@icloud.com>
2015-03-10 19:48:36 +0200

    Initial Commit

---
+++ DelegationObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DelegationObjC
+//
+//  Created by Markku Åkerfelt on 10/03/15.
+//  Copyright (c) 2015 MarakerSoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
